425716def0ff956faadb6ec0dcd235d250eb2acd
Richard Kubíček <rici.kubicek@gmail.com>
2022-08-12 23:15:54 +0200

    Stages 1 & 2 completed

--- Simple Chatty Bot/task/main.js
+++ Simple Chatty Bot/task/main.js
@@ -1,2 +1,15 @@
+const input = require('sync-input');
 
-console.log("Hello, World!");
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2020.");
+console.log("Please, remind me your name.");
+
+let name = input();
+
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+// reading all remainders
+
+console.log("Your age is {your_age}; that's a good time to start programming!");
--- Simple Chatty Bot/task/tests.py
+++ Simple Chatty Bot/task/tests.py
@@ -1,29 +1,46 @@
-import re
 from hstest.stage_test import *
 from hstest.test_case import TestCase
 
 CheckResult.correct = lambda: CheckResult(True, '')
 CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 
-
 class ChattyBotTest(StageTest):
     def generate(self) -> List[TestCase]:
-        return [TestCase()]
+        return [
+            TestCase(stdin="John\n1\n2\n1", attach=("John", 22)),
+            TestCase(stdin="Nick\n2\n0\n0", attach=("Nick", 35))
+        ]
 
     def check(self, reply: str, clue: Any) -> CheckResult:
         lines = reply.strip().splitlines()
-        if len(lines) != 2:
+        if len(lines) != 7:
             return CheckResult.wrong(
-                "You should output exactly 2 lines!\n" +
+                "You should output 7 lines!\n" +
                 f"Lines found: {len(lines)}"
                 f"Your output:\n"
                 f"{reply.strip()}"
             )
 
-        if not re.match(".*\\d.*", lines[1]):
+        line_with_name = lines[3].lower()
+        name = clue[0].lower()
+
+        if name not in line_with_name:
             return CheckResult.wrong(
-                "The second line should contain a year!\n" +
-                "Your second line: \"" + lines[1] + "\""
+                "The name was " + clue[0] + "\n" +
+                "But the 4-th line was:\n" +
+                "\"" + lines[3] + "\"\n\n" +
+                "4-th line should contain a name of the user"
+            )
+
+        line_with_age = lines[6].lower()
+        age = str(clue[1])
+
+        if age not in line_with_age:
+            return CheckResult.wrong(
+                "Can't find a correct age " +
+                "in the last line of output! " +
+                "Maybe you calculated the age wrong?\n\n" +
+                "Your last line: \n" + "\"" + lines[6] + "\""
             )
 
         return CheckResult.correct()

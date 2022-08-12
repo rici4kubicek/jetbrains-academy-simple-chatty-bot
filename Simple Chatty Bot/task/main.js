14521e8bdc3e01838a782e4e81d6ad1aa07df10e
Richard Kubíček <rici.kubicek@gmail.com>
2022-08-12 23:19:00 +0200

    Stage 3 completed

--- Simple Chatty Bot/task/main.js
+++ Simple Chatty Bot/task/main.js
@@ -10,6 +10,8 @@ console.log("What a great name you have, " + name + "!");
 console.log("Let me guess your age.");
 console.log("Enter remainders of dividing your age by 3, 5 and 7.");
 
-// reading all remainders
-
-console.log("Your age is {your_age}; that's a good time to start programming!");
+let remainder3 = input();
+let remainder5 = input();
+let remainder7 = input();
+let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+console.log("Your age is " + age + "; that's a good time to start programming!");
